ebbc630e96a2335c7083e8109c79b27fdefee1dc
sameersheikh2 <sameersheikh819@gmail.com>
2022-10-10 11:00:09 +0530

    add list of map to store userdata

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -10,7 +11,7 @@ var totalTickets int = 50
 var ticketsAvailable uint = 50 //uint var type allow only positive number
 // var bookings [50]string        //this is array.first it need array size and second is value type that comes in array
 // var bookings []string //slice in go, same as array but dynamic and no need to define size as we do in array
-var bookings = []string{}
+var bookings = make([]map[string]string, 0) //map["key data type"]value data type;=syntax;; here we created list of maps inside list of slice thus []map[string]string, 0, where first [] impies empty slice and map[]string means slice of maps
 
 func main() {
 
@@ -28,13 +29,11 @@ func main() {
 		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
-			bookings = append(bookings, firstName+" "+lastName)
-			ticketsAvailable = ticketsAvailable - userTickets
 
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-
+			bookTicket(firstName, lastName, email, userTickets)
 			firstName := firstNames()
 			fmt.Printf("The first name of bookings are %v\n", firstName)
+
 			if ticketsAvailable == 0 {
 				fmt.Println("The tickets are all sold out. Come back next year.")
 				break
@@ -51,8 +50,6 @@ func main() {
 			}
 			continue
 		}
-
-		fmt.Printf("%v tickets remaining for Go Conference.\n", ticketsAvailable)
 
 	}
 
@@ -78,11 +75,36 @@ func getInput() (string, string, string, uint) {
 	return firstName, lastName, email, userTickets
 }
 
+func validateUserInput(firstName, lastName, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= ticketsAvailable
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func bookTicket(firstName string, lastName string, email string, userTickets uint) {
+
+	var userData = make(map[string]string)
+
+	userData["firstName"] = firstName
+	userData["lastName"] = lastName
+	userData["email"] = email
+	userData["bookedTickets"] = strconv.FormatInt(int64(userTickets), 10)
+
+	bookings = append(bookings, userData)
+	ticketsAvailable = ticketsAvailable - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for Go Conference.\n", ticketsAvailable)
+	fmt.Printf("users are:%v\n", userData)
+
+}
+
 func firstNames() []string {
 	firstNames := []string{}
 	for _, booking := range bookings {
-		var names = strings.Fields(booking)       //string.Fields split strings eg. sameer sheikh = "sameer" "sheikh"
-		firstNames = append(firstNames, names[0]) //name holding value eg. "sameer" "sheikh" so name[0] at index 0 we have value sameer
+		// OLD CODE:- var names = strings.Fields(booking)   //string.Fields split strings eg. sameer sheikh = "sameer" "sheikh"
+		firstNames = append(firstNames, booking["firstName"]) //booking holding value eg. [{firsName:"sameer" ,lastName:"sheikh",email:"sameersheikh@gmail.com"}] "sheikh" so name[0] at index 0 we have value sameer
 	}
 	return firstNames
 
@@ -96,11 +118,4 @@ func firstNames() []string {
 	The update updates the value of initialization.
 	The condition is evaluated again. The process continues until the condition is false.
 	If the condition is false, the for loop ends*/
-}
-
-func validateUserInput(firstName, lastName, email string, userTickets uint) (bool, bool, bool) {
-	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= ticketsAvailable
-	return isValidName, isValidEmail, isValidTicketNumber
 }
